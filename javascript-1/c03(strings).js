587e7c3b5263283eea564214015542fb3f8f74b3
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-01-29 17:59:53 +0530

    --

--- javascript-1/c03(strings).js
+++ javascript-1/c03(strings).js
@@ -4,4 +4,11 @@ console.log(a.trim().length);
 a.trim()
 console.log(a.toUpperCase());
 console.log(a.toLowerCase());
+console.log(a.indexOf("@"));
+console.log(a.lastIndexOf("."))
+console.log(a.charAt(5));
+console.log(a.startsWith("s"));
+console.log(a.endsWith("m"));
+console.log(a.startsWith(" "));
+console.log(a.endsWith(" "));
 
