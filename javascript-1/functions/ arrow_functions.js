1289eb510f57af3d7e0fff2860c7696a4acad227
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-03-24 12:31:09 +0530

    some changes in function files

---
+++ javascript-1/functions/ arrow_functions.js
@@ -0,0 +1,21 @@
+
+ let h=setInterval(() => {
+    let o=(c,d)=> c/d;
+    console.log(o(2,4));
+},1000);
+ setTimeout(() => {
+    let mul = (a,b)=>a*b;
+console.log(mul(2,3))
+}, 1000);
+setTimeout(() => {
+    clearInterval(h);
+}, 3000);
+
+let fun=(x,...y)=>{ 
+    //spread operator in functions 
+    console.log(x);
+      console.log(y);
+};
+ fun(1,2,3,4,5,6,7,26,8,46,9,10,11,12,13,14,15);
+
+console.log("hola");
--- javascript-1/functions/functions.js
+++ javascript-1/functions/functions.js
@@ -1,31 +1,12 @@
+//normal function we all know 
 function addi(a,b){
     return a+b;
 }
 console.log(addi(2,3))
 
+//function expression === storing function inside variable
 let add= function addition(a,b){
     return a+b;
-    console.log(a+b);
+    console.log(a+b);// this will not run we saw in c lang also when return comes the function will exit from there itself
 }
 console.log(add("hi","hello"), typeof add())
-
-
- let h=setInterval(() => {
-    let o=(c,d)=> c/d;
-    console.log(o(2,4));
-},1000);
- setTimeout(() => {
-    let mul = (a,b)=>a*b;
-console.log(mul(2,3))
-}, 1000);
-setTimeout(() => {
-    clearInterval(h);
-}, 3000);
-
-let fun=(x,...y)=>{
-    console.log(x);
-      console.log(y);
-};
- fun(1,2,3,4,5,6,7,26,8,46,9,10,11,12,13,14,15);
-
-console.log("hola");
