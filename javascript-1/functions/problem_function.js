b6fbba37b768ac378a24a89106990804dbd9e9c7
SHANAWAJ NADAF <147726542+shanawaj679@users.noreply.github.com>
2026-04-27 12:13:47 +0530

    function_problem

--- javascript-1/functions/problem_function.js
+++ javascript-1/functions/problem_function.js
@@ -3,4 +3,10 @@ function reverse(str){
    let reversed=str.trim().split(/\s+/).reverse().join(" ");
      console.log(`${reversed}`);
 }
+
+function reversed(str){
+    let reversed=str.trim().split("").reverse().join("");
+    console.log(`${reversed}`);
+}
 reverse("hello world");
+reversed("hello world");
